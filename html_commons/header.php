04ac2dddef6c66086dbc41fd2d9c9397763c9a30
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2022-03-14 00:46:40 +0100

    Header desktop and tablet version

--- html_commons/header.php
+++ html_commons/header.php
@@ -13,14 +13,12 @@
 <div class="nav-bar">  
     <header class="container">
         <div class="logo-header">
-            <!-- <span> -->
-                <img src="./assets/images/H_logo.PNG" alt="logo_miranda" />
-                <img src="./assets/images/hotel_miranda_text.png" alt="texto_miranda" />
-            <!-- </span> -->
+            <img src="./assets/images/H_logo.PNG" alt="logo_miranda" />
+            <img src="./assets/images/hotel_miranda_text.png" alt="texto_miranda" />
         </div>
         <nav class="menu-nav">
-            <label for="menu-responsive" class="menu-hamburguer"><i class="fas fa-bars"></i></label>
-            <input type="checkbox" id="menu-responsive">
+            <label for="menu-responsive" class="menu-hamburguer"><i class="fal fa-bars"></i></label>
+            <input type="checkbox" id="menu-responsive" />
             <ul class="menu-items" id="menu-hamburguer-items">
                 <li><a href="404.html">About Us</a></li>
                 <li><a href="404.html">Rooms</a></li>
@@ -28,16 +26,10 @@
                 <li><a href="404.html">Contact</a></li>
             </ul>
         </nav>
-        <!-- <div class="search-nav">
-            <form action="#" method="post">
-                <input type="search" name="searching" id="searching" placeholder="Buscar...">
-                <button type="submit"><i class="fas fa-search"></i></button>
-            </form>
-        </div> -->
         <div class="user-menu">
-            <a href="" class="link-user-menu"><i class="fal fa-user"></i></a>
-            <a href="" class="link-user-menu"><i class="fal fa-search"></i></a>
-            <a href="" class="link-user-menu"><i class="fal fa-bars"></i></a>
+            <a href="" class="link-user-menu" id="user-area"><i class="fal fa-user"></i></a>
+            <a href="" class="link-user-menu" id="searcher"><i class="fal fa-search"></i></a>
+            <a href="" class="link-user-menu" id="menu-bars"><i class="fal fa-bars"></i></a>
         </div>
     </header>
 </div>
