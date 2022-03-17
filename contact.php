950e031e4a377260b6a0e61713424b26464de652
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2022-03-17 18:51:00 +0100

    Contact page first approach

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php include __DIR__.'/html_commons/head_common.html' ?>
 <?php include __DIR__.'/html_commons/header.html' ?>
-<?php include __DIR__.'/html_pages_configs/home_config.html' ?>
-<?php include __DIR__.'/html_pages/home.html' ?>
+<?php include __DIR__.'/html_pages_configs/contact_config.html' ?>
+<?php include __DIR__.'/html_pages/contact.html' ?>
 <?php include __DIR__.'/html_commons/footer.html' ?> 
